8b31ab837318504cd0f0d621a0e403130a6d1701
lhschou <lhschou1@gmail.com>
2022-03-03 10:59:27 +0300

    Add for loop and condition

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,35 @@
 let pokemonList = [
-{
-	name: 'Bulbasaur',
-	height: 0.7,
-	type: ['grass', 'poison']
-},
+	{
+		name: 'Bulbasaur',
+		height: 0.7,
+		type: ['grass', 'poison']
+	},
 
-{
-	name: 'Ivysaur',
-	height: 1,
-	type: ['grass', 'poison']
-},
+	{
+		name: 'Ivysaur',
+		height: 1, 
+		type: ['grass', 'poison']
+	},
 
-{
-	name: 'Wartortle',
-	height: 1,
-	type: ['water']
-}
+	{	
+		name: 'Wartortle',
+		height: 1,
+		type: ['water']
+	},
+
+	{
+		name: 'Dratini',
+		height: '1.8',
+		type: ['dragon']
+	}
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+	document.write(pokemonList[i].name + ' (height: '+ pokemonList[i].height + ') ');
+
+	if (pokemonList[i].height >1.5) {
+		document.write("- Wow that's big!");
+	}
+
+		document.write ('<br/>');
+}
