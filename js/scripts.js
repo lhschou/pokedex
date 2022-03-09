eb2e209824fbd2ca364523accc624a91815c251a
lhschou <100278132+lhschou@users.noreply.github.com>
2022-03-09 11:15:41 +0300

    Edit indentation

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,7 @@ let pokemonRepository = (function () {
 		let listpokemon = document.createElement('li');
 		let button = document.createElement('button');
 		button.innerText = pokemon.name;
-		button.classList.add('button-class')
+		button.classList.add('button-class');
 		listpokemon.appendChild(button);
 		pokemonList.appendChild(listpokemon);
 		button.addEventListener('click', function(event) {
