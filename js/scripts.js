04cd3e83ae785a2f29d126d3121cd3b127444173
lhschou <lhschou1@gmail.com>
2022-03-11 11:25:28 +0300

    Update modal

--- js/scripts.js
+++ js/scripts.js
@@ -94,6 +94,7 @@ let pokemonRepository = (function () {
     modal.appendChild(imageElement);
 	modal.appendChild(titleElement);
     modal.appendChild(contentElement);
+    
     modalContainer.appendChild(modal);
     modalContainer.classList.add('is-visible');
   }
